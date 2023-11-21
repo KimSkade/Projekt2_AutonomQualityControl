6fd103179ad64cc7a28a4d237bd2b9fa2642da41
KimSkade <uuebm@student.kit.edu>
2023-11-21 09:44:01 +0100

    client test

---
+++ aas2openapi-client/get_test.py
@@ -0,0 +1,8 @@
+from aas2openapi_client.api.quality_data_aas.get_items_quality_data_aas_get import sync
+from aas2openapi_client import client
+
+
+client = client.Client(base_url="http://127.0.0.1:8000")
+aas = sync(client=client)
+
+print(aas)
