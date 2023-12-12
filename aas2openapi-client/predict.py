d86840d0e5e24bb7c292c3f3b3c6e775e49223d1
KimSkade <uuebm@student.kit.edu>
2023-12-12 01:44:42 +0100

    idea

---
+++ aas2openapi-client/predict.py
@@ -0,0 +1,9 @@
+# load model from disk
+
+# model = XGboost.load_model_from_disk(path_to_model)
+
+# get new data
+
+# predict on new data
+
+# result = model.predict(new_data)
---
+++ aas2openapi-client/train.py
@@ -0,0 +1,20 @@
+# ML Algos aufrufen und auf dataframe anwenden
+
+# 1. Definition ML ALgo
+# model = xgboost.regression()
+
+# 2. Daten laden
+# data = dataframe.load()
+
+# 3. Daten splitten
+# train, test = sklear.train_test_split(data)
+
+# 4. X, y = train["Spaltennamen"], train["Ziespalten"]
+
+# 5. Model trainieren
+# model.train/fit(X, y)
+
+# prediction = model.predict(test_X)
+
+# model.save(path_in_verawltungssvhale)
+
